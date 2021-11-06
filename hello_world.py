d9da804d9f98b36823d29f126c749972e131e15e
yizhouzhouyi <2283240768@qq.com>
2021-11-06 12:45:13 +0800

    wjm modfify123

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,1 @@
 print("hello world!")
-str1 = "hello world!"
-str2 = "zhouyi"
-f = open('helloworld.html','w')
-message = """
-<html>
-<head></head>
-<body>
-<p>Welcome</p>
-<p>%s</p>
-<h1>%s</h1>
-</body>
-</html>"""%(str1,str2)
-
-f.write(message)
-f.close()
