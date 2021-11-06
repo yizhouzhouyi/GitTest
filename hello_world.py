840dd0cc9e5fa0a02bd54cf944086a677035b3fd
yizhouzhouyi <2283240768@qq.com>
2021-11-06 11:31:28 +0800

    1106modify

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+str1 = "hello world!"
+str2 = "zhouyi"
+f = open('helloworld.html','w')
+message = """
+<html>
+<head></head>
+<body>
+<p>Welcome</p>
+<p>%s</p>
+<h1>%s</h1>
+</body>
+</html>"""%(str1,str2)
+
+f.write(message)
+f.close()
